252cafcbfb091d6d35aa58fb8f62ca5dfa483176
johnkouk469 <johnkouk469@gmail.com>
2022-12-16 09:58:57 +0200

    Tests for feature: Users should be able to view their sources

--- tests/init.spec.js
+++ tests/init.spec.js
@@ -74,6 +74,33 @@ test('GET /sources returns correct response and status code for user admin', asy
   ];
   t.assert(lodash.isEqual(body.sources, expected_source));
   t.is(statusCode, 200);
+  t.assert(body.success);
+  // t.is(body.sources, []);
+});
+
+test('GET /sources returns correct response and status code for user admin', async (t) => {
+  token = jwtSign({username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"});
+  const {body, statusCode} = await t.context.got(`sources/sources?token=${token}`);
+  const expected_source = [
+    {
+      id: "639475b812ff010f4dfc3c16",
+      name: "source1",
+      type: "news",
+      url: "localhost/lalala",
+      login: "lalala",
+      passcode: "",
+      vhost: "/",
+      active: false,
+    },
+  ];
+  t.assert(lodash.isEqual(body.sources, expected_source));
+  t.is(statusCode, 200);
+});
+
+test('GET /sources returns correct response and status code for unauthenticated user', async (t) => {
+  const {body, statusCode} = await t.context.got(`sources/sources`);
+  t.is(statusCode, 403);
+  t.is(body.message, 'Authorization Error: token missing.');
 });
 
 test('GET /sources returns correct response and status code for unauthenticated user', async (t) => {
