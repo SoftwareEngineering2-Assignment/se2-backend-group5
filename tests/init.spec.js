3b7e8833fa0219f7040249d74b28c0b05f4acfd7
johnkouk469 <johnkouk469@gmail.com>
2022-12-27 14:24:30 +0200

    Tests for feature: Users should be able to edit their sources

--- tests/init.spec.js
+++ tests/init.spec.js
@@ -37,3 +37,72 @@ test.after.always(async (t) => {
   await source.deleteMany({});
   await user.deleteMany({});
 });
+
+// test('GET /statistics returns correct response and status code', async (t) => {
+//   const {body, statusCode} = await t.context.got('general/statistics');
+//   t.is(body.sources, (JSON.parse(fs.readFileSync(path.join(path.dirname(__filename), 'mock_data/source.json'), {encoding: 'utf8'}))).length);
+//   t.assert(body.success);
+//   t.is(statusCode, 200);
+// });
+
+// test('GET /sources returns correct response and status code', async (t) => {
+//   const token = jwtSign({id: 1});
+//   const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
+//   t.is(statusCode, 200);
+// });
+
+/*
+ * Tests for route POST /change-source
+ */
+test('POST /change-source returns correct response and status code for existing source owned by the user', async (t) => {
+  const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+  token = jwtSign(mock_user);
+  editted_source = {
+      id: "639475b812ff010f4dfc3c16",
+      name: "source1.2",
+      type: "News",
+      url: "localhost/lalala",
+      login: "lalala",
+      passcode: "",
+      vhost: "/",
+  };
+  const original_source = await source.find({_id: mongoose.Types.ObjectId(editted_source.id), owner: mongoose.Types.ObjectId(mock_user.id)});
+  const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+  const source_after_post = await source.find({_id: mongoose.Types.ObjectId(editted_source.id), owner: mongoose.Types.ObjectId(mock_user.id)});
+  t.falsy(lodash.isEqual(original_source, source_after_post));
+  t.assert(body.success);
+});
+
+test('POST /change-source returns correct response and status code for non existing source', async (t) => {
+  const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+  token = jwtSign(mock_user);
+  editted_source = {
+      id: "639475b812f0010f4dfc3c16",
+      name: "source1.2",
+      type: "News",
+      url: "localhost/lalala",
+      login: "lalala",
+      passcode: "",
+      vhost: "/",
+  };
+  const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+  t.is(body.status, 409);
+  t.is(body.message, 'The selected source has not been found.');
+});
+
+test('POST /change-source returns correct response and status code for existing source owned by the user when the user already has another source with the new name', async (t) => {
+  const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+  token = jwtSign(mock_user);
+  editted_source = {
+      id: "639475b812ff010f4dfc3c16",
+      name: "source3",
+      type: "News",
+      url: "localhost/lalala",
+      login: "lalala",
+      passcode: "",
+      vhost: "/",
+  };
+  const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+  t.is(body.status, 409);
+  t.is(body.message, 'A source with the same name has been found.');
+});
