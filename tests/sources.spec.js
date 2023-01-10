9428be8a4f875ada2ea55a1467cdf9298dab0c2a
johnkouk469 <johnkouk469@gmail.com>
2023-01-10 20:09:26 +0200

    Tests for route: POST /create-source

--- tests/sources.spec.js
+++ tests/sources.spec.js
@@ -40,3 +40,46 @@ test.after.always(async (t) => {
 test('should be successful', async (t) => {
     t.assert(true);
 });
+
+/*
+ * Tests for route POST /create-source
+ */
+test.serial('POST /create-source returns correct response and status code for non-existing source', async (t) => {
+    const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+    token = jwtSign(mock_user);
+    new_source = {     
+        name: "newSource",      
+        type: "News",
+        url: "localhost/lalalala", 
+        login: "lalala",
+        passcode: "",
+        vhost: "/",
+    };
+    const original_source = await source.find({ owner: mongoose.Types.ObjectId(mock_user.id), name: new_source.name});
+    t.is(JSON.stringify(original_source), "[]");
+    const {body, statusCode} = await t.context.got.post(`sources/create-source?token=${token}`,  {json: new_source});
+    const source_after_post = await source.find({ owner: mongoose.Types.ObjectId(mock_user.id), name: new_source.name});
+    t.is(source_after_post[0].name, new_source.name);
+    t.is(source_after_post[0].type, new_source.type);
+    t.is(source_after_post[0].login, new_source.login);
+    t.assert(body.success);
+  });
+
+test.serial('POST /create-source returns correct response and status code when a source with the same name already exists', async (t) => {
+    const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+    token = jwtSign(mock_user);
+    new_source = {     
+        name: "source1",      
+        type: "News",
+        url: "localhost/lalalala", 
+        login: "lalala",
+        passcode: "",
+        vhost: "/",
+    };
+    const existing_source = await source.find({ owner: mongoose.Types.ObjectId(mock_user.id), name: new_source.name});
+    t.is(existing_source[0].name, new_source.name);
+    const {body, statusCode} = await t.context.got.post(`sources/create-source?token=${token}`,  {json: new_source});
+    t.is(body.status, 409);
+    t.is(body.message, 'A source with that name already exists.');
+    t.is(statusCode, 200);
+  });
