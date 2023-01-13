4c3042052d7320b3a67efc36e2b2707eb73cffef
johnkouk469 <johnkouk469@gmail.com>
2023-01-13 20:02:30 +0200

    Move tests for feature edit sources into sources.spec.js

--- tests/sources.spec.js
+++ tests/sources.spec.js
@@ -78,3 +78,59 @@ test('GET /sources returns correct response and status code for unauthenticated 
     t.is(statusCode, 403);
     t.is(body.message, 'Authorization Error: token missing.');
   });
+
+/*
+ * Tests for route POST /change-source
+ */
+test.serial('POST /change-source returns correct response and status code for existing source owned by the user', async (t) => {
+    const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+    token = jwtSign(mock_user);
+    editted_source = {
+        id: "639475b812ff010f4dfc3c16",
+        name: "source1.2",
+        type: "News",
+        url: "localhost/lalala",
+        login: "lalala",
+        passcode: "",
+        vhost: "/",
+    };
+    const original_source = await source.find({_id: mongoose.Types.ObjectId(editted_source.id), owner: mongoose.Types.ObjectId(mock_user.id)});
+    const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+    const source_after_post = await source.find({_id: mongoose.Types.ObjectId(editted_source.id), owner: mongoose.Types.ObjectId(mock_user.id)});
+    t.falsy(lodash.isEqual(original_source, source_after_post));
+    t.assert(body.success);
+  });
+
+test.serial('POST /change-source returns correct response and status code for non existing source', async (t) => {
+    const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+    token = jwtSign(mock_user);
+    editted_source = {
+        id: "639475b812f0010f4dfc3c16",
+        name: "source1.2",
+        type: "News",
+        url: "localhost/lalala",
+        login: "lalala",
+        passcode: "",
+        vhost: "/",
+    };
+    const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+    t.is(body.status, 409);
+    t.is(body.message, 'The selected source has not been found.');
+  });
+
+test.serial('POST /change-source returns correct response and status code for existing source owned by the user when the user already has another source with the new name', async (t) => {
+    const mock_user = {username: "admin", id: "6394753012ff010f4dfc3c12", email: "admin@example.com"};
+    token = jwtSign(mock_user);
+    editted_source = {
+        id: "639475b812ff010f4dfc3c16",
+        name: "source3",
+        type: "News",
+        url: "localhost/lalala",
+        login: "lalala",
+        passcode: "",
+        vhost: "/",
+    };
+    const {body, statusCode} = await t.context.got.post(`sources/change-source?token=${token}`,  {json: editted_source});
+    t.is(body.status, 409);
+    t.is(body.message, 'A source with the same name has been found.');
+  });
